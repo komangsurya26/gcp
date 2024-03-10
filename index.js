49513580315bbe74d9b0a6fa8b64a705f2ba619d
komangsuryasedana <komangsuryasedana@mail.com>
2024-03-10 16:43:28 +0800

    edit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-
 const app = express()
+require('dotenv').config()
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    return res.send('Hello World!')
 })
 
 //listen
